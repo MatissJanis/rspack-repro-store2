64a5858db2f3a74882a6370c998d7e0072cc6747
Matiss Janis Aboltins <matissjanis.aboltins@datadoghq.com>
2025-01-22 11:14:51 +0000

    Reproduction

--- src/render.js
+++ src/render.js
@@ -1,8 +1,14 @@
+import store2 from 'store2';
+
 import './render.css'
+
+const retrievedValue = store2.get('hello');
 
 export function render() {
     const el = document.createElement('div')
     el.classList.add('text')
     document.getElementsByTagName('body')[0].appendChild(el)
     el.innerHTML = 'hello, world'
+
+    console.log(retrievedValue)
 }
